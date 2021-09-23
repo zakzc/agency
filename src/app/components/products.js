adc24fa242308fa5f0c4c307a346f1ee01fd89cc
zakzc <zak.zangrando@outlook.com>
2021-09-23 21:47:04 +0200

    Updates layout and starts including some data

--- src/app/components/products.js
+++ src/app/components/products.js
@@ -1,9 +1,24 @@
 import React from "react";
+//
+import data from "../data/data.json";
 
 const Products = () => {
+  const ProductDescription = ({ name, price, status }) => (
+    <>
+      <h4>{name}</h4>
+      <p>{price}</p>
+      <p>{status}</p>
+    </>
+  );
+
   return (
     <div className="products">
-      <h3>Products here </h3>
+      <h3>Produtos em Destaque</h3>
+      {data.map((d, index) => (
+        <div key={index}>
+          <ProductDescription name={d.name} price={d.price} status={d.status} />
+        </div>
+      ))}
     </div>
   );
 };
