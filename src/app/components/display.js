915799b07f2fe5b9e091cece42cb4d3bf919e63c
zakzc <zak.zangrando@outlook.com>
2021-09-27 14:19:02 +0200

    Adds low text over image in display banner

--- src/app/components/display.js
+++ src/app/components/display.js
@@ -4,20 +4,23 @@ import Zelda from "../assets/img/zelda_banner.jpg";
 import Sekiro from "../assets/img/sekiro_banner.jpg";
 
 const Display = () => {
-  const DisplayText = () => {
-    return <span className="display-text">Some text goes here</span>;
+  const DisplayText = ({ text }) => {
+    return <span className="display-text">{text}</span>;
   };
   return (
     <div className="display" style={{ marginTop: "-50px" }}>
       <div className="flex-container">
-        <div className="flex-item-left">
-          <img src={Zelda} alt="Zelda game" className="image-style" />
-          {/* <DisplayText /> */}
+        <div className="image-container">
+          <div className="flex-item-left">
+            <img src={Zelda} alt="Zelda game" className="image-style" />
+            <DisplayText text={"The Legend of Zelda - Breath of the wild"} />
+          </div>
         </div>
-
-        <div className="flex-item-right">
-          <img src={Sekiro} alt="Sekiro game" className="image-style" />
-          {/* <DisplayText /> */}
+        <div className="flex-container">
+          <div className="flex-item-right">
+            <img src={Sekiro} alt="Sekiro game" className="image-style" />
+            <DisplayText text={"SEKIRO - Shadows die twice"} />
+          </div>
         </div>
       </div>
     </div>
